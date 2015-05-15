d8e640eb5e930b9572e94ef9990b7921d40b4028
Guillaume AMAT <guillaume@amat.io>
2015-05-16 01:17:34 +0200

    Basic settings handling. We have to log in to see the edit button and we can set a new title and a new default position (center and zoom) but the profile is hardcoded (id) and it's loaded from the wrong file. Anyway, it's a start ;)

---
+++ db/data.js
@@ -0,0 +1,19 @@
+
+db.dropDatabase();
+
+
+
+
+db.profile.insert({
+
+	'_id' : ObjectId('5249c43c6e789470197b5973'),
+	'name': 'Rudomap',
+	'pois': [],
+	'tiles': [],
+	'zoomLevel': 12,
+	'center': {
+
+		'lat': 44.82921,
+		'lng': -0.5834,
+	},
+});
---
+++ db/import_database.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mongo openmapper data.js
