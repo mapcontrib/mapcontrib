f6a97e9b06c6c050f65e2d6f2926f992acb08896
Guillaume AMAT <guillaume@amat.io>
2015-05-21 01:56:59 +0200

    Display a custom icon for markers

--- src/public/js/view/main.js
+++ src/public/js/view/main.js
@@ -373,7 +373,21 @@ function (
 
 								self._map.addLayer(
 
-									L.marker(pos).bindPopup(popupContent)
+									L.marker(pos, {
+
+										'icon': L.icon({
+
+											iconUrl: 'img/leaf-green.png',
+											shadowUrl: 'img/leaf-shadow.png',
+
+											iconSize:     [38, 95], // size of the icon
+											shadowSize:   [50, 64], // size of the shadow
+											iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+											shadowAnchor: [4, 62],  // the same for the shadow
+											popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+										})
+									})
+									.bindPopup(popupContent)
 								);
 							});
 						}
