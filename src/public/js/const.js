a7315749edbc376dbb254ffbdc7599ab4cd59310
Guillaume AMAT <guillaume@amat.io>
2015-06-11 14:26:35 +0200

    Add marker-3

--- src/public/js/const.js
+++ src/public/js/const.js
@@ -4,10 +4,12 @@ define([
 
 	'text!img/markers/1_optimized.svg',
 	'text!img/markers/2_optimized.svg',
+	'text!img/markers/3_optimized.svg',
 ], function (
 
 	marker1,
-	marker2
+	marker2,
+	marker3
 ) {
 
 	'use strict';
@@ -47,6 +49,14 @@ define([
 					'popupAnchor':  [0, -38],
 					'className': 'marker marker-2',
 					'html': marker2,
+				},
+				'marker3': {
+
+					'iconSize':     [36, 42],
+					'iconAnchor':   [18, 42],
+					'popupAnchor':  [0, -38],
+					'className': 'marker marker-3',
+					'html': marker3,
 				},
 			}
 		},
