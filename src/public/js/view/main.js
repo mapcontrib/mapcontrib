af04e2ffd195c1869ae1a127c7adc3c6b5c9c468
Guillaume AMAT <guillaume@amat.io>
2016-02-25 20:07:34 +0100

    Obsolete condition

--- src/public/js/view/main.js
+++ src/public/js/view/main.js
@@ -302,11 +302,6 @@ function (
                 if ( this.model.isOwner(userModel) === true ) {
 
                     this.showEditTools();
-
-                    if ( this.isLargeScreen() ) {
-
-                        this.ui.editToolbar.addClass('open');
-                    }
                 }
             }
             else {
