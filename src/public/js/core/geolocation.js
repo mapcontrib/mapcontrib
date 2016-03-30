b4722d26e24d2ee457486158558b6096eb17e119
Guillaume AMAT <guillaume@amat.io>
2016-03-30 07:59:57 +0200

    Fixes the first geolocation try

--- src/public/js/core/geolocation.js
+++ src/public/js/core/geolocation.js
@@ -13,9 +13,6 @@ export default class Geolocation {
     locate() {
         this._dragged = false;
 
-        this.stopLocate();
-
-        this.removeEventListeners();
         this.addEventListeners();
 
         this._map.locate({
