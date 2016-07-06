2cecc908f86eecbea419d7c58cc93d5543fbbdf6
Guillaume AMAT <guillaume@amat.io>
2016-07-06 21:42:52 +0200

    Adds the non-OSM data model

---
+++ src/public/js/model/nonOsmData.js
@@ -0,0 +1,18 @@
+
+import Backbone from 'backbone';
+
+
+export default Backbone.Model.extend({
+    defaults: {
+        'creationDate': new Date().toISOString(),
+        'modificationDate': new Date().toISOString(),
+        'osmId': undefined,
+        'osmType': undefined,
+        'userId': undefined,
+        'tags': [], // { key: string, value: string, type: text|img }
+    },
+
+    updateModificationDate: function () {
+        this.set('modificationDate', new Date().toISOString());
+    },
+});
