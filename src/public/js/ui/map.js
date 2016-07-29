163a7d02d481f8fcec230ea1d789d6f73edf48e4
Guillaume AMAT <guillaume@amat.io>
2016-07-29 18:04:52 +0200

    Makes POI less opaque when contributing

--- src/public/js/ui/map.js
+++ src/public/js/ui/map.js
@@ -13,7 +13,7 @@ export default class MapUi {
      */
     static showContributionCross () {
         document.body.classList.add('contribution_cross_visible');
-        document.querySelector('.leaflet-marker-pane').style.display = 'none';
+        document.querySelector('.leaflet-marker-pane').style.opacity = 0.7;
     }
 
     /**
@@ -25,7 +25,7 @@ export default class MapUi {
      */
     static hideContributionCross () {
         document.body.classList.remove('contribution_cross_visible');
-        document.querySelector('.leaflet-marker-pane').style.display = 'block';
+        document.querySelector('.leaflet-marker-pane').style.opacity = 1;
     }
 
     /**
