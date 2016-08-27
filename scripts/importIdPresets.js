5bcff4c03ad810588d42acb1a3d7520f16192aa1
Guillaume AMAT <guillaume@amat.io>
2016-08-27 10:57:21 +0200

    Start of import iD presets script

---
+++ scripts/importIdPresets.js
@@ -0,0 +1,29 @@
+import presetsBuilder from 'id-presets-builder';
+import argp from 'argp';
+
+
+const argv = argp.createParser({ once: true })
+    .description ('Import presets, categories, fields and defaults from an iD reporitory clone')
+    .on('end', function (argv) {
+        if ( !argv.dir ) {
+            this.printUsage(1);
+        }
+    })
+    .on('error', function (error) {
+        this.fail(error);
+    })
+    .body ()
+        .text(' Options:')
+        .option({
+            description: 'iD\'s repository clone directory',
+            short: 'd',
+            long: 'dir',
+            metavar: 'DIR',
+            optional: false,
+            type: String
+        })
+        .usage()
+        .argv();
+
+
+// presetsBuilder.generatePresets(argv, callback);
