caa0798f796a2d12338781e9ea615fcf7363923e
Guillaume AMAT <guillaume@amat.io>
2016-09-05 20:53:09 +0200

    Removes debug lines

--- public/js/view/contribFormColumn.js
+++ public/js/view/contribFormColumn.js
@@ -204,9 +204,7 @@ export default Marionette.LayoutView.extend({
         this._osmEdit.setUid(this.options.user.get('osmId'));
         this._osmEdit.setDisplayName(this.options.user.get('displayName'));
 
-        console.log(this._osmEdit.getOverPassElement());
-        // this.sendContributionToOSM();
-        this.ui.footerButtons.prop('disabled', false); // A VIRER
+        this.sendContributionToOSM();
     },
 
     sendContributionToOSM() {
