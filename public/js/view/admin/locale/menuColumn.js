7a9391f74aea76b197a30e6883a170c8c3dff511
Guillaume AMAT <guillaume@amat.io>
2016-10-12 19:58:55 +0200

    Start the column creation

---
+++ public/js/view/admin/locale/menuColumn.js
@@ -0,0 +1,70 @@
+
+import Wreqr from 'backbone.wreqr';
+import Marionette from 'backbone.marionette';
+import template from 'templates/admin/setting/menuColumn.ejs';
+import SearchList from 'ui/form/searchList';
+
+
+export default Marionette.ItemView.extend({
+    template,
+
+    behaviors() {
+        return {
+            l20n: {},
+            column: {
+                appendToBody: true,
+                destroyOnClose: true,
+                routeOnClose: this.options.previousRoute,
+            },
+        };
+    },
+
+    ui: {
+        column: '.column',
+    },
+
+    initialize() {
+        this._radio = Wreqr.radio.channel('global');
+    },
+
+    onRender() {
+        const searchLocales = new SearchList({
+            items: [
+                {
+                    label: 'Français',
+                    progression: 100,
+                    href: '#',
+                    callback: undefined,
+                },
+                {
+                    label: 'Anglais',
+                    progression: 65,
+                    href: '#',
+                    callback: undefined,
+                },
+                {
+                    label: 'Italien',
+                    progression: 40,
+                    href: '#',
+                    callback: undefined,
+                },
+            ],
+        });
+        this.getRegion('locales').show(searchLocales);
+    },
+
+    onBeforeOpen() {
+        this._radio.vent.trigger('column:closeAll', [ this.cid ]);
+        this._radio.vent.trigger('widget:closeAll', [ this.cid ]);
+    },
+
+    open() {
+        this.triggerMethod('open');
+        return this;
+    },
+
+    close() {
+        this.triggerMethod('close');
+        return this;
+    },
+});
