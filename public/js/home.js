fdd737c2ae3fc570ce8033ccc577c09faecb0674
Guillaume AMAT <guillaume@amat.io>
2016-10-12 18:25:41 +0200

    Remove the font face observer

--- public/js/home.js
+++ public/js/home.js
@@ -2,16 +2,13 @@
 import App from './app';
 import Router from 'router/home';
 import '../css/home.less';
-import FontLoader from 'core/fontLoader';
 import redirectToHttps from 'helper/forceHttps';
 
 
-FontLoader.load().then(() => {
-    document.l10n.ready(() => {
-        if (MAPCONTRIB.config.forceHttps) {
-            return redirectToHttps(MAPCONTRIB.config.forceHttps);
-        }
+document.l10n.ready(() => {
+    if (MAPCONTRIB.config.forceHttps) {
+        return redirectToHttps(MAPCONTRIB.config.forceHttps);
+    }
 
-        return new App(window).start(Router);
-    });
+    return new App(window).start(Router);
 });
--- public/js/theme.js
+++ public/js/theme.js
@@ -2,16 +2,13 @@
 import App from './app';
 import Router from 'router/theme';
 import '../css/theme.less';
-import FontLoader from 'core/fontLoader';
 import redirectToHttps from 'helper/forceHttps';
 
 
-FontLoader.load().then(() => {
-    document.l10n.ready(() => {
-        if (MAPCONTRIB.config.forceHttps) {
-            return redirectToHttps(MAPCONTRIB.config.forceHttps);
-        }
+document.l10n.ready(() => {
+    if (MAPCONTRIB.config.forceHttps) {
+        return redirectToHttps(MAPCONTRIB.config.forceHttps);
+    }
 
-        return new App(window).start(Router);
-    });
+    return new App(window).start(Router);
 });
