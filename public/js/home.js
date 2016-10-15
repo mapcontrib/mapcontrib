634f70203a0f585f702d31b9cbf3097debd564fb
Guillaume AMAT <guillaume@amat.io>
2016-10-15 14:06:40 +0200

    Redirect to https on the server side

--- public/js/home.js
+++ public/js/home.js
@@ -2,13 +2,6 @@
 import App from './app';
 import Router from 'router/home';
 import '../css/home.less';
-import redirectToHttps from 'helper/forceHttps';
 
 
-document.l10n.ready(() => {
-    if (MAPCONTRIB.config.forceHttps) {
-        redirectToHttps(MAPCONTRIB.config.forceHttps);
-    }
-
-    return new App(window).start(Router);
-});
+document.l10n.ready(() => new App(window).start(Router));
