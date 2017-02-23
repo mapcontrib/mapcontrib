93f32e16be4183eb18651b9358029dd49dcf4a77
Guillaume AMAT <guillaume@amat.io>
2017-02-23 22:23:05 +0100

    All the tiles are always available when clicking on a « display more » button in the tile selection column.

--- CHANGELOG_DEV.md
+++ CHANGELOG_DEV.md
@@ -1,3 +1,4 @@
 # Changelog of the current development version
 
 * The OSM monochrome, Watercolor and Mapbox street satellite tiles are now enanled by default.
+* All the tiles are always available when clicking on a « display more » button in the tile selection column.
--- public/js/view/select/tileColumn.js
+++ public/js/view/select/tileColumn.js
@@ -25,14 +25,38 @@ export default Marionette.LayoutView.extend({
         column: '.column',
         tileList: '.tile_list',
         tiles: '.tile_list input',
+        displayMore: '.display_more',
     },
 
     events: {
         'click @ui.tiles': 'onClickTiles',
+        'click @ui.displayMore': 'onClickdisplayMore',
     },
 
     initialize() {
         this._radio = Wreqr.radio.channel('global');
+
+        this.listenTo(this.model, 'change:tiles', this.onChangeModelTiles);
+
+        this._radio.vent.on('theme:rendered', this.onRender.bind(this));
+
+        this._moreTilesDisplayed = false;
+    },
+
+    onRender() {
+        let tile;
+        let checked;
+        let html = '';
+        let tiles = this.model.get('tiles');
+
+        if (this._moreTilesDisplayed) {
+            this.ui.displayMore.addClass('hide');
+
+            tiles = Object.keys(CONST.map.tiles);
+        }
+        else {
+            this.ui.displayMore.removeClass('hide');
+        }
 
         this._fragment = this._radio.reqres.request('theme:fragment');
         const storage = JSON.parse( localStorage.getItem(`mapState-${this._fragment}`) ) || {};
@@ -40,15 +64,6 @@ export default Marionette.LayoutView.extend({
         if ( storage.selectedTile ) {
             this._selectedInStorage = storage.selectedTile;
         }
-
-        this.listenTo(this.model, 'change:tiles', this.onChangeModelTiles);
-    },
-
-    onRender() {
-        let tile;
-        let checked;
-        let html = '';
-        const tiles = this.model.get('tiles');
 
         tiles.forEach((id) => {
             tile = CONST.map.tiles[id];
@@ -72,9 +87,6 @@ export default Marionette.LayoutView.extend({
             }
 
             if ( this._selectedInStorage && this._selectedInStorage === id ) {
-                checked = ' checked';
-            }
-            else if (id === tiles[0]) {
                 checked = ' checked';
             }
             else {
@@ -107,8 +119,13 @@ export default Marionette.LayoutView.extend({
         this._radio.commands.execute('map:setTileLayer', e.target.value);
     },
 
+    onClickdisplayMore() {
+        this._moreTilesDisplayed = true;
+        this.onRender();
+    },
+
     onChangeModelTiles() {
-        this.render();
+        this.onRender();
     },
 
     onBeforeOpen() {
