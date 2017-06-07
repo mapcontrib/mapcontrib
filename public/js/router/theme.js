fc90246ce717071e704d58d3f2228d8550274566
Guillaume AMAT <guillaume@amat.io>
2017-06-07 11:48:18 +0200

    Fix the map positionning when the position is given in the url

--- public/js/router/theme.js
+++ public/js/router/theme.js
@@ -172,12 +172,7 @@ export default Backbone.Router.extend({
     },
 
     routeMapPosition(zoom, lat, lng) {
-        const setPosition = () => this._radio.commands.execute('map:position', zoom, lat, lng);
-
-        if (setPosition() === false) {
-            this._radio.vent.on('theme:rendered', setPosition);
-        }
-
+        this._radio.commands.execute('map:position', zoom, lat, lng);
         this.navigate('');
     },
 
